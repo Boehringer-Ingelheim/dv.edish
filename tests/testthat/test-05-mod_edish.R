c49bc75096bbfc38c154f2cfe13671fef93970b2
Mark Luff <mark.luff.ext@boehringer-ingelheim.com>
2025-05-02 12:04:47 +0100

    Debugging test

--- tests/testthat/test-05-mod_edish.R
+++ tests/testthat/test-05-mod_edish.R
@@ -72,6 +72,7 @@ test_that("the app's state is restored when bookmarking" %>%
 
   # Initialize bookmarked app
   bmk_url <- app_bmk$get_value(export = "url")
+  warning(bmk_url)
   app_rst <- shinytest2::AppDriver$new(app_dir = bmk_url, name = "test_restoring")
 
   app_rst$wait_for_idle()
