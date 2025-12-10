5ea75545482ff9b1494538d27c45089bcc580932
Mark Luff <mark.luff.ext@boehringer-ingelheim.com>
2025-12-10 02:53:59 +0000

    Remove magrittr pipe from setup.R

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -24,7 +24,7 @@ specs <- vdoc[["specs"]]
 #'                         should make the module send `dv.papo` a message.
 test_communication_with_papo <- function(mod, data, trigger_input_id) {
   datasets <- shiny::reactive(data)
-  
+
   afmm <- list(
     data = list(DS = data),
     unfiltered_dataset = datasets,
@@ -34,36 +34,36 @@ test_communication_with_papo <- function(mod, data, trigger_input_id) {
     utils = list(switch2mod = function(id) NULL),
     dataset_metadata = list(name = shiny::reactive("dummy_dataset_name"))
   )
-  
+
   app_ui <- function() {
     shiny::fluidPage(mod[["ui"]](mod[["module_id"]]))
   }
-  
+
   app_server <- function(input, output, session) {
     ret_value <- mod[["server"]](afmm)
-    
+
     ret_value_update_count <- shiny::reactiveVal(0)
     shiny::observeEvent(ret_value[["subj_id"]](), ret_value_update_count(ret_value_update_count() + 1))
-    
+
     shiny::exportTestValues(
       ret_value = try(ret_value[["subj_id"]]()), # try because of https://github.com/rstudio/shiny/issues/3768
       update_count = ret_value_update_count()
     )
     return(ret_value)
   }
-  
+
   app <- shiny::shinyApp(ui = app_ui, server = app_server)
-  
-  testthat::test_that("module adheres to send_subject_id_to_papo protocol" %>% 
-    vdoc[["add_spec"]](specs$framework_specs$jumping_feature), { 
+
+  testthat::test_that("module adheres to send_subject_id_to_papo protocol" |>
+    vdoc[["add_spec"]](specs$framework_specs$jumping_feature), {
     app <- shinytest2::AppDriver$new(app, name = "test_send_subject_id_to_papo_protocol")
-                     
+
     app$wait_for_idle()
-    
+
     # Module starts and sends no message
     exports <- app$get_values()[["export"]]
     testthat::expect_equal(exports[["update_count"]], 0)
-    
+
     trigger_subject_selection <- function(subject_id) {
       set_input_params <- append(
         as.list(setNames(paste0("[{\"key\":\"", subject_id, "\"}]"), trigger_input_id)),
@@ -71,7 +71,7 @@ test_communication_with_papo <- function(mod, data, trigger_input_id) {
       )
       do.call(app$set_inputs, set_input_params)
     }
-    
+
     # Module sends exactly one message per trigger event, even if subject does not change
     subject_ids <- c("A", "A", "B")
     for (i in seq_along(subject_ids)) {
@@ -83,7 +83,7 @@ test_communication_with_papo <- function(mod, data, trigger_input_id) {
       testthat::expect_equal(exports[["ret_value"]], subject_ids[[i]])
       testthat::expect_equal(exports[["update_count"]], i)
     }
-    
-    app$stop()      
+
+    app$stop()
   })
 }
