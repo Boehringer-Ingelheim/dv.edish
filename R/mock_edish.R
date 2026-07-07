a054be429a843e9820bcea5e4694aca5b7f93960
Mark Luff <mark.luff.ext@boehringer-ingelheim.com>
2026-07-07 08:20:13 +0000

    Export mock functions (but keep them internal)

--- R/mock_edish.R
+++ R/mock_edish.R
@@ -3,6 +3,7 @@
 #' `mock_edish_app()` launches a mock app containing the dv.edish shiny module.
 #'
 #' @keywords internal
+#' @export
 mock_edish_app <- function() {
   dm <- pharmaverseadam::adsl |> dplyr::mutate(dplyr::across(dplyr::where(is.character), as.factor))
   lb <- pharmaverseadam::adlb |> dplyr::mutate(dplyr::across(dplyr::where(is.character), as.factor))
@@ -43,6 +44,7 @@ mock_edish_app <- function() {
 #' `mock_edish_mm()` launches a mock app containing the dv.edish shiny module by means of the `dv.manager`.
 #'
 #' @keywords internal
+#' @export
 mock_edish_mm <- function() {
   dm <- pharmaverseadam::adsl
   lb <- pharmaverseadam::adlb
